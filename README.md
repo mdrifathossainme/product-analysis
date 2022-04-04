17b45122869aded2b7ff8053ace96eeac52a8b44
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-04-04 22:04:53 +0600

    complete

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# Assignment  link: [code link :](https://github.com/programming-hero-web-course-4/product-analysis-website-mdrifathossainme#readme)
+# Assignment  link: 
+## [code link :](https://github.com/programming-hero-web-course-4/product-analysis-website-mdrifathossainme#readme)
  
- [Live Link:](https://marvelous-kringle-acae0a.netlify.app/).
+ ## [Live Link:](https://marvelous-kringle-acae0a.netlify.app/).
 
  
 
-## What am I doing from this project?
+# What am I doing from this project?
 
-#### Setup React Router
-#### Create Router menu bar
-#### Add Custom Link in menu bar
-#### Create 404 page
-#### Create rechart 
-#### Use destractureing
-#### Create Custom hook
-#### use slice element.
-#### use tailwind css Framework
-#### Responsive Site
-#### Create custom review APi
+## Setup React Router
+## Create Router menu bar
+## Add Custom Link in menu bar
+## Create 404 page
+## Create rechart 
+## Use destractureing
+## Create Custom hook
+## use slice element.
+## use tailwind css Framework
+## Responsive Site
+## Create custom review APi
 
--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            Blogs
-Blogs
+        <div className='lg:mx-8  lg:mt-12 sm:mx-3 sm:mt-4'>
+            <h2 className='text-4xl'>what is is context API ?</h2>
+            <p>React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier</p>
+
+            <p className='mt-1'>This new API solves one major problem–prop drilling. Prop drilling is the processing of getting data from component A to component Z by passing it through multiple layers of intermediary React components. Component will receive props indirectly and you, the React Developer will have to ensure everything works out right.</p>
+
+            <h2 className='text-4xl lg:mt-8'>What Is Semantic  Tag ?</h2>
+            <p>Semantic HTML or semantic markup is HTML that introduces meaning to the web page rather than just presentation. For example, a (p ) tag indicates that the enclosed text is a paragraph. This is both semantic and presentational because people know what paragraphs are, and browsers know how to display them.</p>
+
+          <p className='mt-1'>  On the flip side of this equation, tags such as (b) and (i)  tag are not semantic. They define only how the text should look (bold or italic), and don't provide any additional meaning to the markup.</p>
         </div>
     );
 };
