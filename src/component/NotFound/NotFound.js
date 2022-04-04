810fac86e4a5979c1254c7ec0076d35d3f8e3f24
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-04-04 15:11:30 +0600

    404 page done

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            404
+        <div className='notFound'>
+          <h1 className='text-center text-9xl mt-16 text-yellow-300'>Page Not Foud!</h1>
         </div>
     );
 };
